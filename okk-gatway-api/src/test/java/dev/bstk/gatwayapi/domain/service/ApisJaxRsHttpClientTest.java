2a45134bdb65baf03c4d35ff90567738a79799fa
Bruno Luz <bsilva.se@gmail.com>
2022-06-07 12:42:59 -0300

    main - Adicionando caos de testes para ulr inválida ao ApisJaxRsHttpClient

--- okk-gatway-api/src/test/java/dev/bstk/gatwayapi/domain/service/ApisJaxRsHttpClientTest.java
+++ okk-gatway-api/src/test/java/dev/bstk/gatwayapi/domain/service/ApisJaxRsHttpClientTest.java
@@ -2,10 +2,14 @@ package dev.bstk.gatwayapi.domain.service;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.mockito.Mock;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -17,6 +21,7 @@ import java.util.Map;
 import static org.mockito.ArgumentMatchers.anyString;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class ApisJaxRsHttpClientTest {
 
     @Mock
@@ -67,5 +72,70 @@ class ApisJaxRsHttpClientTest {
 
         Mockito.verify(webTarget, Mockito.times(map.size())).queryParam(anyString(), anyString());
         Mockito.verify(builder, Mockito.times(map.size())).header(anyString(), anyString());
+    }
+
+    @Test
+    @DisplayName("Deve retonar um Jax Invocation.Builder com QueryParams")
+    void deveRetonarUmJaxInvocationBuilderComQueryParams() {
+        final Map<String, String> map = Map.of("chave_A", "valor_A", "chave_B", "valor_B");
+
+        final Invocation.Builder requestBuild = ApisJaxRsHttpClient
+            .Builder
+            .builder()
+            .url("https://mockurl.com.br")
+            .queryParams(map)
+            .build();
+
+        Assertions.assertNotNull(requestBuild);
+
+        Mockito.verify(webTarget, Mockito.times(map.size())).queryParam(anyString(), anyString());
+        Mockito.verify(builder, Mockito.never()).header(anyString(), anyString());
+    }
+
+    @Test
+    @DisplayName("Deve retonar um Jax Invocation.Builder com Headers")
+    void deveRetonarUmJaxInvocationBuilderComHeaders() {
+        final Map<String, String> map = Map.of("chave_A", "valor_A", "chave_B", "valor_B");
+
+        final Invocation.Builder requestBuild = ApisJaxRsHttpClient
+            .Builder
+            .builder()
+            .url("https://mockurl.com.br")
+            .headers(map)
+            .build();
+
+        Assertions.assertNotNull(requestBuild);
+
+        Mockito.verify(webTarget, Mockito.never()).queryParam(anyString(), anyString());
+        Mockito.verify(builder, Mockito.times(map.size())).header(anyString(), anyString());
+    }
+
+    @Test
+    @DisplayName("Deve lançar excesão de url invalida quando não setar a url")
+    void deveLancarExcesaoDeUrlInvalidaAoNaoInformarUrl() {
+        final ApisJaxRsHttpClient.Builder builder = ApisJaxRsHttpClient.Builder.builder();
+
+        final Exception exception = Assertions
+            .assertThrows(
+                IllegalArgumentException.class,
+                builder::build
+            );
+
+        Assertions.assertEquals("Url não pode ser nula ou vazia!", exception.getMessage());
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("Deve lançar excesão de url invalida [Nula / Vazia]")
+    void deveLancarExcesaoDeUrlInvalida(final String url) {
+        final ApisJaxRsHttpClient.Builder builder = ApisJaxRsHttpClient.Builder.builder();
+
+        final Exception exception = Assertions
+            .assertThrows(
+                IllegalArgumentException.class,
+                () -> builder.url(url)
+            );
+
+        Assertions.assertEquals("Url não pode ser nula ou vazia!", exception.getMessage());
     }
 }
