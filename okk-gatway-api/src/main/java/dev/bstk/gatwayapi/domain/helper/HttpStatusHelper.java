722e57774a1f473cccc7a39df4ff2bed7411de36
Bruno Luz <bsilva.se@gmail.com>
2022-06-08 09:29:17 -0300

    main - Refatorando testes unitário HttpStatusHelper

--- okk-gatway-api/src/main/java/dev/bstk/gatwayapi/domain/helper/HttpStatusHelper.java
+++ okk-gatway-api/src/main/java/dev/bstk/gatwayapi/domain/helper/HttpStatusHelper.java
@@ -1,24 +1,24 @@
 package dev.bstk.gatwayapi.domain.helper;
 
-import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status.Family;
 
 public final class HttpStatusHelper {
 
     private HttpStatusHelper() { }
 
     public static boolean ok(final int statusCode) {
-        return Response.Status.OK.getStatusCode() == statusCode;
+        return Family.SUCCESSFUL.equals(Family.familyOf(statusCode));
     }
 
     public static boolean nok(final int statusCode) {
         return clientError(statusCode) || serverError(statusCode);
     }
 
-    public static boolean serverError(final int statusCode) {
-        return Response.Status.INTERNAL_SERVER_ERROR.getStatusCode() == statusCode;
+    public static boolean clientError(final int statusCode) {
+        return Family.CLIENT_ERROR.equals(Family.familyOf(statusCode));
     }
 
-    public static boolean clientError(final int statusCode) {
-        return Response.Status.BAD_REQUEST.getStatusCode() == statusCode;
+    public static boolean serverError(final int statusCode) {
+        return Family.SERVER_ERROR.equals(Family.familyOf(statusCode));
     }
 }
--- okk-gatway-api/src/test/java/dev/bstk/gatwayapi/domain/helper/HttpStatusHelperTest.java
+++ okk-gatway-api/src/test/java/dev/bstk/gatwayapi/domain/helper/HttpStatusHelperTest.java
@@ -1,80 +1,88 @@
 package dev.bstk.gatwayapi.domain.helper;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import javax.ws.rs.core.Response;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @TestMethodOrder(MethodOrderer.MethodName.class)
 class HttpStatusHelperTest {
 
-    @Test
-    @DisplayName("Deve retonar [ TRUE ] para requisicao com [ STATUS_OK ]")
-    void deveRetonarTrueParaRequisicaoComSucessoOK() {
-        Assertions.assertTrue(HttpStatusHelper.ok(Response.Status.OK.getStatusCode()));
+    private static final int HTTP_STATUS_CODE_SUCESSO = 2;
+    private static final int HTTP_STATUS_CODE_ERRO_CLEINTE = 4;
+    private static final int HTTP_STATUS_CODE_ERRO_SERVIDOR = 5;
+
+    @MethodSource("dadosMassaTesteStatusHttpSucesso")
+    @DisplayName("Bateria de testes para requisição com sucesso")
+    @ParameterizedTest(name = "Deve retonar [ TRUE ] para requisicao com status code: {0}")
+    void deveRetonarTrueParaRequisicaoComSucessoOK(final Response.Status status) {
+        Assertions.assertTrue(HttpStatusHelper.ok(status.getStatusCode()));
     }
 
-    @Test
-    @DisplayName("Deve retonar [ TRUE ] para requisicao com [ STATUS_NOK ]")
-    void deveRetonarTrueParaRequisicaoComSucessoNOK() {
-        Assertions.assertTrue(HttpStatusHelper.nok(Response.Status.BAD_REQUEST.getStatusCode()));
-        Assertions.assertTrue(HttpStatusHelper.nok(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()));
+    @MethodSource("dadosMassaTesteStatusHttpErroClienteServidor")
+    @DisplayName("Bateria de testes para requisição com erro cliente/servidor")
+    @ParameterizedTest(name = "Deve retonar [ TRUE ] para requisicao erro cliente/servidor e status code: {0}")
+    void deveRetonarTrueParaRequisicaoComSucessoNOK(final Response.Status status) {
+        Assertions.assertTrue(HttpStatusHelper.nok(status.getStatusCode()));
     }
 
-    @Test
-    @DisplayName("Deve retonar [ TRUE ] para requisicao com [ STATUS_INTERNAL_SERVER_ERROR ]")
-    void deveRetonarTrueParaRequisicaoComServerErrorInternalServerError() {
-        Assertions.assertTrue(HttpStatusHelper.serverError(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()));
+    @MethodSource("dadosMassaTesteStatusHttpErroCliente")
+    @DisplayName("Bateria de testes para requisição com erro do cliente")
+    @ParameterizedTest(name = "Deve retonar [ TRUE ] para requisicao com status code: {0}")
+    void deveRetonarTrueParaRequisicaoComClientErrorBadRequest(final Response.Status status) {
+        Assertions.assertTrue(HttpStatusHelper.clientError(status.getStatusCode()));
     }
 
-    @Test
-    @DisplayName("Deve retonar [ TRUE ] para requisicao com [ STATUS_BAD_REQUEST ]")
-    void deveRetonarTrueParaRequisicaoComClientErrorBadRequest() {
-        Assertions.assertTrue(HttpStatusHelper.clientError(Response.Status.BAD_REQUEST.getStatusCode()));
+    @MethodSource("dadosMassaTesteStatusHttpErroServidor")
+    @DisplayName("Bateria de testes para requisição com erro do servidor")
+    @ParameterizedTest(name = "Deve retonar [ TRUE ] para requisicao com status code: {0}")
+    void deveRetonarTrueParaRequisicaoComServerErrorInternalServerError(final Response.Status status) {
+        Assertions.assertTrue(HttpStatusHelper.serverError(status.getStatusCode()));
     }
 
-    @ParameterizedTest
-    @EnumSource(
-        value = Response.Status.class,
-        names = "OK",
-        mode = EnumSource.Mode.EXCLUDE
-    )
-    @DisplayName("Deve executar bateria de testes [ FALSE ] para [ STATUS_OK ]")
-    void deveExecutarBateriaDeTestesFalseParaRequisicaoComSucessoOK(final Response.Status status) {
-        Assertions.assertFalse(HttpStatusHelper.ok(status.getStatusCode()));
+    private static Stream<Arguments> dadosMassaTesteStatusHttpSucesso() {
+        return mapArgumentsStream(HTTP_STATUS_CODE_SUCESSO);
     }
 
-    @ParameterizedTest
-    @EnumSource(
-        value = Response.Status.class,
-        names = { "BAD_REQUEST", "INTERNAL_SERVER_ERROR" },
-        mode = EnumSource.Mode.EXCLUDE
-    )
-    @DisplayName("Deve executar bateria de testes [ FALSE ] para [ STATUS_NOK ]")
-    void deveExecutarBateriaDeTestesFalseParaRequisicaoComSucessoNOK(final Response.Status status) {
-        Assertions.assertFalse(HttpStatusHelper.nok(status.getStatusCode()));
+    private static Stream<Arguments> dadosMassaTesteStatusHttpErroCliente() {
+        return mapArgumentsStream(HTTP_STATUS_CODE_ERRO_CLEINTE);
     }
 
-    @ParameterizedTest
-    @EnumSource(
-        value = Response.Status.class,
-        names = "BAD_REQUEST",
-        mode = EnumSource.Mode.EXCLUDE
-    )
-    @DisplayName("Deve executar bateria de testes [ FALSE ] para [ STATUS_BAD_REQUEST ]")
-    void deveExecutarBateriaDeTestesFalseParaRequisicaoComClientErrorBadRequest(final Response.Status status) {
-        Assertions.assertFalse(HttpStatusHelper.clientError(status.getStatusCode()));
+    private static Stream<Arguments> dadosMassaTesteStatusHttpErroServidor() {
+        return mapArgumentsStream(HTTP_STATUS_CODE_ERRO_SERVIDOR);
     }
 
-    @ParameterizedTest
-    @EnumSource(
-        value = Response.Status.class,
-        names = "INTERNAL_SERVER_ERROR",
-        mode = EnumSource.Mode.EXCLUDE
-    )
-    @DisplayName("Deve executar bateria de testes [ FALSE ] para [ STATUS_INTERNAL_SERVER_ERROR ]")
-    void deveExecutarBateriaDeTestesFalseParaRequisicaoComInternalServerError(final Response.Status status) {
-        Assertions.assertFalse(HttpStatusHelper.serverError(status.getStatusCode()));
+    private static Stream<Arguments> dadosMassaTesteStatusHttpErroClienteServidor() {
+        final Stream.Builder<Arguments> builder = Stream.builder();
+
+        mapHttpStatus(HTTP_STATUS_CODE_ERRO_CLEINTE).forEach(status -> builder.add(Arguments.of(status)));
+        mapHttpStatus(HTTP_STATUS_CODE_ERRO_SERVIDOR).forEach(status -> builder.add(Arguments.of(status)));
+
+        return builder.build();
+    }
+
+    private static Stream<Arguments> mapArgumentsStream(final int httpStatusCodeMultiplo) {
+        final Stream.Builder<Arguments> builder = Stream.builder();
+
+        mapHttpStatus(httpStatusCodeMultiplo)
+            .forEach(status -> builder.add(Arguments.of(status)));
+
+        return builder.build();
+    }
+
+    private static List<Response.Status> mapHttpStatus(final int httpStatusCodeMultiplo) {
+        final Response.Status[] todosStatusCode = Response.Status.values();
+        return Arrays.stream(todosStatusCode)
+            .filter(status -> status.getStatusCode() / 100 == httpStatusCodeMultiplo)
+            .collect(Collectors.toList());
     }
 }
