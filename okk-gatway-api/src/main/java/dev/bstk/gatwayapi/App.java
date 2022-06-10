fb3a5c623a0967969af4b182fdf7df84f651d348
Bruno Luz <bsilva.se@gmail.com>
2022-06-09 21:18:29 -0300

    main - Adicionando documentação Sawagger

--- okk-gatway-api/src/main/java/dev/bstk/gatwayapi/App.java
+++ okk-gatway-api/src/main/java/dev/bstk/gatwayapi/App.java
@@ -1,7 +1,23 @@
 package dev.bstk.gatwayapi;
 
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Contact;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Okk Gateway Api Service",
+        description = "Microserviço para obter dados das api's apara seus clientes",
+        version = "1.0.0",
+        contact = @Contact(
+            name = "bstk",
+            url = "https://github.com/bstk",
+            email = "bsilva.se@gmail.com"
+        )
+    )
+)
 @ApplicationPath("/okk-gatway")
 public class App extends Application { }
